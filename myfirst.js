c901db308c9fa3cdbfb42fa0e08d84c9a3f62f52
Cedric L. Williams Jr <clw.jr90@gmail.com>
2023-03-24 07:52:07 -0500

    updated date response

--- myfirst.js
+++ myfirst.js
@@ -3,7 +3,6 @@ var dt = require('./myfirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello there!');
     res.write('The current date & time are: ' + dt.myDateTime());
     res.end();
 }).listen(8080);
