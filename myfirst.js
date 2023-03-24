da3bdd92b17bee2f3ebf086935322f992c75bf3d
Cedric L. Williams Jr <clw.jr90@gmail.com>
2023-03-23 19:14:44 -0500

    removed greeting

--- myfirst.js
+++ myfirst.js
@@ -3,6 +3,7 @@ var dt = require('./myfirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('The date & time are currently: ' + dt.myDateTime());
+    res.write('Hello there!');
+    res.write('The current date & time are: ' + dt.myDateTime());
     res.end();
 }).listen(8080);
