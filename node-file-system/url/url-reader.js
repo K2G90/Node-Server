aaba3532dcffab4572f0c6cf6cd2030af164df13
Cedric L. Williams Jr <clw.jr90@gmail.com>
2023-03-25 16:53:12 -0500

    update url to read files

---
+++ node-file-system/url/url-reader.js
@@ -0,0 +1,19 @@
+var url = require('url');
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+    var q = url.parse(req.url, true);
+    var file = "." + q.pathname;
+
+    fs.readFile(file, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+             return res.end('Page not found'); //without return it will not explicity display the text to screen
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    })
+}).listen(8080);
+
