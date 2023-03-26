92ac4466ddf2a5c6fc3df0cc5ce0a3dec0672414
Cedric L. Williams Jr <clw.jr90@gmail.com>
2023-03-25 21:36:55 -0500

    implemented upload module..still an error with path/destination

--- node-file-system/uploadfs/form.js
+++ node-file-system/uploadfs/form.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function(req, res){
-    if(req.url == 'fileupload') {
+    if(req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files){
-            res.write('File Upload');
-            res.end();
+            var oldpath = files.filetoupload.filepath;
+            var newpath = 'C:/Users/clwjr/' + files.filetoupload.originalFilename;
+            fs.rename(oldpath, newpath, function(err){
+                if(err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
         });
     } else{
         res.writeHead(200, {'Content-Type': 'text/html'});
